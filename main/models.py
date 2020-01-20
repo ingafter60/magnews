30a379abe47b5fa13d05081aadc955501b238cae
ing <inyoman_gurnitha@yahoo.com>
2020-01-20 07:03:18 +0700

    Sec 24 - 1. Created main model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
+# unicode_literals for any languages possible
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Main(models.Model):
+
+	name = models.TextField()
+
+	def __str__(self):
+		return self.name

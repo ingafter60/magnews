742bf2e6a1f1c94aff2f4ec002cfad26353abf98
ing <inyoman_gurnitha@yahoo.com>
2020-01-20 07:34:51 +0700

    Sec 25 - 1. Added dynamic pagetitle from db

--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Main(models.Model):
 
 	name = models.TextField()
+	about = models.TextField(null=True)
 
 	def __str__(self):
 		return self.name
--- main/views.py
+++ main/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.shortcuts import render
 
+from .models import Main
+
 # Create your views here.
 
 def home(request):
-	return render(request, 'front/home.html', {})
+
+	# pagetitle = 'Home | MAGNews'
+	pagetitle = Main.objects.get(pk=2)
+
+	return render(request, 'front/home.html', {'pagetitle':pagetitle})
 
 def about(request):
-	return render(request, 'front/about.html', {})
+
+	# pagetitle = 'About | MAGNews'
+	pagetitle = Main.objects.get(pk=2)
+
+	return render(request, 'front/about.html', {'pagetitle':pagetitle})
